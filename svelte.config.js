8bae8efc97b1e582808d7bcbab2a3f4bcd777831
Arujjwal <arujjwal73@gmail.com>
2024-04-08 12:50:12 +0530

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
-const dev = process.argv.includes('dev');
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		appDir: 'app',
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
-	},
-	preprocess: vitePreprocess()
+			// see below for options that can be set here
+		})
+	}
 };
-
-export default config;
